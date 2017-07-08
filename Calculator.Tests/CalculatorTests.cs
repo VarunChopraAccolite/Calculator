030ceb96d091529a2443c1b19b8b006cb374730e
VarunChopraAccolite <varun.chopra@accolitelabs.com>
2017-07-08 19:11:04 +0530

    Add project files.

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddTwoNumbers()
+        {
+            // Arrange
+            var sut = new SimpleCalculator();
+
+            // Act
+            var result = sut.Add(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldMultiplyTwoNumbers()
+        {
+            // Arrange
+            var sut = new SimpleCalculator();
+
+            // Act
+            var result = sut.Multiply(1, 2);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
